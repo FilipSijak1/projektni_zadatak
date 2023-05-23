32ce126b1a28c0ab00f2cb890f382a50b731cc11
FilipSijak1 <126612710+FilipSijak1@users.noreply.github.com>
2023-05-23 15:34:27 +0200

    Filip.Sijak.6.pz.rc2

--- artikl/Vozilo.py
+++ artikl/Vozilo.py
@@ -1,9 +1,9 @@
-class Vozilo():
-    class Vozilo:
 
-        def __izracunaj_kw(self, snaga):
-            return int(snaga * 0.735)
+class Vozilo:
 
-        def cijena_osiguranja(self, snaga):
-            kilovati = self.__izracunaj_kw(snaga)
-            return kilovati * 15
+    def __izracunaj_kw(self, snaga):
+        return int(snaga * 0.735)
+
+    def cijena_osiguranja(self, snaga):
+        kilovati = self.__izracunaj_kw(snaga)
+        return kilovati * 15
